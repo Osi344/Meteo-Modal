da3a334132acf96fe8072e072491cc82a86f5a72
Osi344 <bosquet.do22@gmail.com>
2019-12-28 22:49:14 +0100

    work in progress about searching cities

--- script.js
+++ script.js
@@ -1,5 +1,68 @@
+
+class Cities {
+    constructor() {
+        // this.data = respsonse;
+        this.index = 0;
+        this.list = [];
+    }
+
+    addCityToList(newCity) {
+        this['list'][this['index']].push(newCity);
+    }
+
+    removeCityFromList() {
+        if (this.index > 1) {
+            this['list'][this['index']] = [];
+            this.index--;
+        }
+    }
+}
+
+class City {
+    constructor(name, url, index) {
+        this.name = name;
+        this.url = url;
+        this.index = index;
+    }
+    // 18 / name: Affligem
+    // 18 / npa: 1790
+    // 18 / region: 
+    // 18 / country: BEL
+    // 18 / url: affligem
+
+    // dispatchData() {
+    //     console.log("dispatchData");
+    //     for (let key in this.data) {
+    //         switch (key) {
+    //             case "name":
+    //                 this.name = key;
+    //                 break;
+    //             case "npa":
+    //                 this.npa = key;
+    //                 break;
+    //             case "region":
+    //                 this.region = key;
+    //                 break;
+    //             case "url":
+    //                 this.url = key;
+    //                 break;
+    //             default:
+    //                 break;
+    //         }
+    //     }
+    //     this.nameLength = this.name.length;
+    // }
+
+    // show() {
+    //     console.log(`name:${this['name']}`);
+    //     console.log(`npa:${this['npa']}`);
+    //     console.log(`region:${this['region']}`);
+    //     console.log(`url:${this['url']}`);
+    // }
+}
+
 class MeteoCard {
-    constructor (parent,index,response){
+    constructor(parent, index, response) {
 
         this.parent = parent;
         this.dayNumber = `fcst_day_${index}`;
@@ -7,7 +70,7 @@ class MeteoCard {
         this.dayName = `${response[this.dayNumber]['day_long']} ${response[this.dayNumber]['date']}`;
         this.condition = `${response[this.dayNumber]['condition']}`;
         this.imgSrc = `${response[this.dayNumber]['icon']}`;
-        if (index == 0 ){
+        if (index == 0) {
             this.imgSrc = `${response[this.dayNumber]['icon_big']}`;
             this.hour = `${response['current_condition']['hour']}`;
             this.temp = `Temp: ${response['current_condition']['tmp']}°C`;
@@ -19,12 +82,13 @@ class MeteoCard {
             this.tempMin = `Tmin: ${response[this.dayNumber]['tmin']}°C`;
             this.tempMax = `Tmax: ${response[this.dayNumber]['tmax']}°C`;
         }
+
     }
-   
-    updateCurrentCard(){
+
+    updateCurrentCard() {
 
         // image a centrer verticalement
-        this.content= 
+        this.content =
             `<div class="card w-80 text-center">
                 <div class="row no-gutters">
                     <div class="col-md-4">
@@ -43,8 +107,8 @@ class MeteoCard {
             </div>`;
     }
 
-    updateNextCard(){
-        this.content= 
+    updateNextCard() {
+        this.content =
             `<div class="card w-25 text-center">
                 <img src="${this.imgSrc}" class="card-img-top" alt="bottom">
                 <div class="card-body">
@@ -56,25 +120,26 @@ class MeteoCard {
             </div>`;
     }
 }
-
+let myCities = new Cities();
+let countryFilter = "FRA";
 // https://cors-anywhere.herokuapp.com/https://www.prevision-meteo.ch/services/json/list-cities
 
-function updateInfo(response){
-       
+function updateInfo(response) {
+
     let cardArray = [];
-    for (let i=0; i<5; i++) {
-        
+    for (let i = 0; i < 5; i++) {
+
         // parent
-        let parent= document.querySelector('#nextMeteo');
+        let parent = document.querySelector('#nextMeteo');
         if (i == 0) {
-            parent= document.querySelector('#currentMeteo');
+            parent = document.querySelector('#currentMeteo');
         }
 
         // nouvel objet
-        let newCard = new MeteoCard(parent,i,response);
+        let newCard = new MeteoCard(parent, i, response);
 
         // current
-        if (i==0) {
+        if (i == 0) {
             newCard.updateCurrentCard();
             parent.innerHTML = `${newCard['content']}`;
         }
@@ -87,19 +152,85 @@ function updateInfo(response){
 
     // prevision
     let newChildren = "";
-    let parent= document.querySelector('#nextMeteo');
-    for (let i=0; i<4; i++) {
-        newChildren+= `${cardArray[i]['content']}`;
+    let parent = document.querySelector('#nextMeteo');
+    for (let i = 0; i < 4; i++) {
+        newChildren += `${cardArray[i]['content']}`;
     }
     parent.innerHTML = newChildren;
 }
 
-// async method
-const getMeteoAsync = async function (address) {
-    const response = await fetch(`${address}`);
-    const jsonData = await response.json();
-    console.log(jsonData);
-    updateInfo(jsonData);
+function getCities(list) {
+    let myBool= 0;
+    for (let i in list) {
+        for (let j in list[i]) {
+            // countryFilter
+            if ((j == "country") && (list[i][j] == "FRA")) {
+                const newCity= new City(list[i]['name'], list[i]['url'], i);
+                myCities.addCityToList(newCity);
+                myBool= 1;
+            }
+        }
+        // if(k>30) {
+        // break;
+        // }
+    }
+    if (myBool) {
+        myCities.index++;
+    }
 }
 
-getMeteoAsync("https://www.prevision-meteo.ch/services/json/toulon");
+function checkCities(string) {
+    let lowString = string.toLowerCase();
+    let regex = `/^(${lowString}).*/`;
+
+    for (let city of myCities['list'][myCities['index']]) {
+        let lowName = city['name'].toLowerCase();
+        var foundB = lowName.match(regex);
+        if (foundB) {
+            console.log(`\tcities: " + ${city['name']}`);
+            if (city['name'] === string) {
+                console.log("\t\tsuper");
+            }
+        }
+    }
+}
+
+// sync = async function () {
+//     const response = await fetch("https://cors-anywhere.herokuapp.com/https://www.prevision-meteo.ch/services/json/list-cities");
+//     const jsonData = await response.json();
+//     let i=0;
+//     for(let a in jsonData) {
+//         console.log("a: " + jsonData[a]);
+//         i++;
+//         if (i == 10) {
+//             break;
+//         }
+//     }   
+//     citiesObject = new Cities(jsonData);
+//     // return jsonData;
+// }
+
+// async function
+const getMeteoAsync = async function (address, choice) {
+    const response = await fetch(address);
+    const jsonData = await response.json();
+    if (choice == 1) {
+        console.log(jsonData);
+        updateInfo(jsonData);
+    }
+    else {
+        getCities(jsonData);
+    }
+}
+
+getMeteoAsync("https://cors-anywhere.herokuapp.com/https://www.prevision-meteo.ch/services/json/list-cities", 0);
+getMeteoAsync("https://www.prevision-meteo.ch/services/json/toulon", 1);
+
+let cityEntry = document.getElementById('inputSearch');
+cityEntry.addEventListener('input', function (e) {
+    if (e.target.value.length > 2) {
+        checkCities(e.target.value)
+
+        console.log("Entry: " + e.target.value);
+    }
+});
